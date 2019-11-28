ff08cbaae61e0a77948d4602a1505c56469e2505
saha777 <alexfootbig@gmail.com>
2019-11-28 12:04:06 +0200

    Add files via upload
    finite state machine

--- main.c
+++ main.c
@@ -1,45 +1,70 @@
-#include "buffer.h"
 #include "delay.h"
-#include "buffer.h"
+#include "keyboard.h"
 #include "tm1637.h"
-// Test buffer and display
-// Display Tm 1637 shuld be:GNG-GND, VCC-5v, DIO - B6, CLC - B7
+#include "number_converter.h"
 
-//#define BUF_SIZE_128 128
-//#define BUF_SIZE_64  64
-//#define BUF_SIZE_16  16
-//#define BUF_SIZE_8   8
-//#define BUF_SIZE_4   4
-//// Two GPIO for SPI emul //GPIO_Pin_6|GPIO_Pin_7;//GPIOB
+#include "stdlib.h"
+#include "stm32f10x.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
 
 
+//keyboard
+//C3 GPIOB9
+//C2 GPIOB10
+//C1 GPIOB11
+//R4 GPIOB12
+//R3 GPIOB13
+//R2 GPIOB14
+//R1 GPIOB15
 
-unsigned int digit_display0 = 0;
-unsigned int digit_display2 = 2222;
+//display
+//CLK GPIOB6
+//DIO GPIOB7
+//UCC 5V
+//GND G
 
-
-
+int state = 0;
+unsigned int keypad_columns[3] = 
+{
+	GPIO_Pin_9,
+	GPIO_Pin_10,
+	GPIO_Pin_11
+};
+unsigned int keypad_rows[4] = 
+{
+	GPIO_Pin_12,
+	GPIO_Pin_13,
+	GPIO_Pin_14,
+	GPIO_Pin_15
+};
 
 int main()
 {
-
 	TM1637_init();	
-  TM1637_brightness(BRIGHT_TYPICAL); 
-	delay_ms(10);
-	TM1637_display_all(digit_display0);
-	delay_ms(1000);
-	TM1637_display_all(digit_display2);
-	delay_ms(1000);
-	for (int i=0; i<BUF_SIZE_8; i++)
-	{
-			buffer_add(i);
-	}
-
+  TM1637_brightness(BRIGHT_TYPICAL);
+	keypad_init(keypad_columns, keypad_rows);
+  delay_ms(10);
 	while (1)
 	{
-		TM1637_display_all(buffer_read()); 
-		delay_ms(300);
+		char key=keypadGetKey();
+		int number = -1;;
+		
+		switch(key) {
+			case '*':
+				number = state * 10;
+				break;
+			case '#':
+				number = state / 10;
+				break;
+			default:
+				number = convert_char(key);
+		}
+		
+		if (number != -1) {
+			state = number;
+			TM1637_display_all(state);
+		}
+		delay_ms(1);
 	}
 }
-
-
